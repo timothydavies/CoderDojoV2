d7a20d05fccf1199794dada70edb8f2ec4488de8
JasmineLei <xylei372@gmail.com>
2016-03-27 14:31:42 +0800

    gig

--- WebRTC/test/ff.js
+++ WebRTC/test/ff.js
@@ -32,14 +32,14 @@ describe('test mentor static feedback', function() {
     before(function(done) {
 		
 		ninjaSocket = io('https://localhost:8000',{forceNew: true});
-        browserB
+        browserB//.pause(1000)
                 .init(done)
                 .windowHandleSize({width: 1000, height: 800})
                         .url('https://localhost:8000/sign_in?url=%2FMentor')
                         .then(function(){
                             ninjaSocket.emit('iceRequest', {mentor:'Test Ninja'});
                         })
-                        .pause(1000)
+                        //.pause(1000)
                         .call(done);
 	});
     after(function() {
@@ -50,21 +50,21 @@ describe('test mentor static feedback', function() {
     it('should fill email and password and login as mentor', function(done) {
         browserB.setValue('#email', 'jj')
                         .setValue('#password', '123')
-                        .click('.btn').pause(1000)
+                        .click('.btn')//.pause(1000)
                         .getTitle().then(function(title){
                             title.should.equal('Mentor Toolbar');
                         })
-                        .pause(2000)
+                        //.pause(2000)
                         .call(done);
              
     });
     
     it('ninja should request', function(done) {
 			
-			browserB.pause(500).then(function(){
+			browserB.getTitle().then(function(){
                         
                         ninjaSocket.emit('requestHelp');
-                    }).pause(1000)
+                    })//.pause(1000)
                     .getHTML('#helpQueue .btn',false).then(function(ele){
                         ele.should.exist;
                     })
@@ -72,35 +72,33 @@ describe('test mentor static feedback', function() {
 	});
     
     it('mentor should answer', function(done) {
-			       browserB.click('#helpQueue .btn').pause(1000)
+			       browserB.click('#helpQueue .btn')//.pause(1000)
                            .getHTML('#headingThree h4 a',false).then(function(ele){
                                ele.should.equal('Chats');
                            })
-                           .pause(1000)
+                           //.pause(1000)
                            .call(done);
 	});
     
     it('should add video', function(done) {
             
-	browserB.pause(1000).then(function(){
+	browserB.getTitle().then(function(){
                 ninjaSocket.emit('test_addVideo');
             })
             .getHTML('#ninjaScreen',false).then(function(ele){
                         ele.should.exist;
                     })
-            .pause(1000)
+            //.pause(1000)
             .call(done);
 	});
     
     it('should take screenshot', function(done) {
             
-            browserB.pause(1000)
-                            
-                           .click('#takescreenShot').pause(1000)
+            browserB.click('#takescreenShot')
                             .isVisible('#myCanvas').then(function(isVisible) {
                               isVisible.should.equal(true);
                             }) 
-                            .pause(2000)
+                            //.pause(2000)
                             .call(done);
                          
 	});
@@ -110,25 +108,25 @@ describe('test mentor static feedback', function() {
     
     it('should cancel current screenshot and take a new one', function(done) {
 
-                browserB.click('#closeCanvas').pause(1000)
+                browserB.click('#closeCanvas')//.pause(1000)
                         .isVisible('#myCanvas').then(function(isVisible) {
                             isVisible.should.equal(false);
                         }) 
-                        .click('#takescreenShot').pause(1000)
+                        .click('#takescreenShot')//.pause(1000)
                         .isVisible('#myCanvas').then(function(isVisible) {
                             isVisible.should.equal(true);
                         })
-                        .pause(3000)
+                        //.pause(3000)
                         .call(done);  
 	});
     
     it('should highlight screenshot', function(done) {
         ninjaSocket.emit('test_highlight');
-         browserB.pause(1000)
+         browserB//.pause(1000)
                        .getAttribute('#myCanvas', 'textContent').then(function(txtContent) {
                             txtContent.should.equal('changed'); 
                         })
-                        .pause(3000)
+                        //.pause(3000)
                         .call(done);
     });
     it('should send out image', function(done) {
@@ -138,26 +136,25 @@ describe('test mentor static feedback', function() {
             data.should.have.property('name');
             //console.log(data);
         }
-        browserB.click('#sendscreenShot').pause(2000)
+        browserB.click('#sendscreenShot')//.pause(2000)
                        .isExisting('img.fancybox').then(function(isExisting) {
                           isExisting.should.equal(true); 
-                         })
-                         .pause(2000);  
+                         });
+                         //.pause(2000);  
                          
         ninjaSocket.once('screenshot',test);
-        browserB.pause(1000)
-                .call(done);
+        browserB.call(done);
 	});
     
     it('should sign out', function(done) {
         
          browserB.click('#signOut')
-                        .pause(2000)
+                        //.pause(2000)
                         .call(done);
 	});
 
     it('should end the session', function(done) {
-         browserB.pause(2000)
+         browserB//.pause(2000)
                        .end()
                        .call(done);
     });
