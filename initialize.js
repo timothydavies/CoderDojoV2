bb5f1a15ad868fad6915ad7d46556aaa888c7cb6
JasmineLei <xylei372@gmail.com>
2016-04-10 22:35:21 +0800

    jb

--- initialize.js
+++ initialize.js
@@ -14,22 +14,22 @@ for (var j=0; j<roles.length;j++){
 var item1 = { "_id" : "1",
              "firstName" : "admin", 
              "lastName" : "", 
-             "roles" : ["Administrator" ], 
+             "roles" : ["Administrator"], 
              "email" : "admin", 
              "password" : "123", 
              "username" : "admin" };
 var item2 = { "_id" : "2",
              "firstName" : "jj", 
              "lastName" : "l", 
-             "roles" : ["Mentor" ], 
+             "roles" : ["Mentor"], 
              "email" : "jj", 
              "password" : "123", 
              "username" : "jj" };
 
 var item3 = { "_id" : "3",
              "firstName" : "c", 
-             "lastName" : "l", 
-             "roles" : ["Champion" ], 
+             "lastName" : "lkk", 
+             "roles" : ["Champion"], 
              "dojos" : ["146546"],
              "email" : "c", 
              "password" : "123", 
@@ -48,6 +48,7 @@ var meeting1 = {
 db.user.insert(item1);
 db.user.insert(item2);
 db.dojo.insert(session1);
+db.meeting.insert(meeting1);
 db.user.insert(item3);
 
-db.meeting.insert(meeting1);
+
